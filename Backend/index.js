1d07c0212b3d08c1999f98166ef03fd9cd38c86b
SashenkaG <goonewardanes@gmail.com>
2024-12-18 16:19:25 +0530

    Frontend files with react router dom

--- Backend/index.js
+++ Backend/index.js
@@ -3,11 +3,15 @@ import {PORT, mongoDBURL} from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import booksRoute from './routes/booksRoute.js';
+import cors from'cors';
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+//middleware for handling CORS POLICY
+app.use(cors());
 
 app.get('/', (request, response) => {
     return response.status(234).send('Welcome');
