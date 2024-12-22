6c29219abddc03279c308f46b4439bf8ffe20cd5
SashenkaG <goonewardanes@gmail.com>
2024-12-22 15:40:03 +0530

    Show books page debugging

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import spinner from "../components/spinner";
+import Spinner from "../components/spinner";
 import { Link } from "react-router-dom";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
@@ -33,7 +33,7 @@ const Home = () => {
         </Link>
       </div>
       {loading ? (
-        <spinner />
+        <Spinner />
       ) : (
         <table className="w-full border-seperate border-spacing-2">
           <thead>
--- Frontend/src/pages/ShowBooks.jsx
+++ Frontend/src/pages/ShowBooks.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import BackButton from '../components/BackButton';
+import Spinner from '../components/spinner';
 
 
 const ShowBooks = () => {
@@ -27,7 +28,7 @@ const ShowBooks = () => {
       <BackButton />
       <h1 className='text-3xl my-4'>Show Book</h1>
       {loading ? (
-        <spinner />
+        <Spinner />
       ) : (
         <div className='flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
           <div className='my-4'>
