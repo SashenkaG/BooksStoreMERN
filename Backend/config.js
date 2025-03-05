5b5bfc333907d574968fb195a44afb1ca575ebd7
Sashenka Goonewardane <159514411+SashenkaG@users.noreply.github.com>
2025-03-05 16:09:25 +0530

    Update config.js

--- Backend/config.js
+++ Backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555; 
 
-export const mongoDBURL = "mongodb+srv://root:root@cluster0.osv47.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+export const mongoDBURL = ""
